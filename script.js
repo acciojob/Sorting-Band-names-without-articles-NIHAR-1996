a9af3b4c0d07cdfe89e848296e9caa76ac986b3d
NIHAR-1996 <131172201+NIHAR-1996@users.noreply.github.com>
2023-06-17 12:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 //your code here
-//1.Array of band names
-let bandNames=['The virupaksha temple','Victoria Memorial','Tajmahal'];
-//2.Remove Articles from Band Names by map Function
-let modifiednames=bandNames.map(names=>names.replace(/^(a|an|the)\s/i""));
-//3.Sorting the modified names lexicographically
-modifiednames.sort();
-//creating an HTML list
-let ul=document.createElement('ul');
-ul.setAttribute('id','band');
-modifiednames.forEach(name=>{
-	let li=document.createElemnt('li');
-	li.textContent=name;
-	ul.appendChild(li);
-})
-document.body.appendChild(ul);
+let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones'];
 
+// Remove articles ('a', 'an', 'the') from band names and sort them in lexicographic order
+let modifiedNames = bandNames.map(name => name.replace(/^(a|an|the)\s/i, ''))
+                             .sort((a, b) => a.localeCompare(b));
+
+// Create a <ul> element with id='band'
+let ul = document.createElement('ul');
+ul.setAttribute('id', 'band');
+
+// Add each band name as an <li> element to the <ul> element
+modifiedNames.forEach(name => {
+  let li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
+
+// Append the <ul> element to the document body or a desired container
+document.body.appendChild(ul);
 
 
 
