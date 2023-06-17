82045bacd9878900e11fc45311f77975c85d9eb1
NIHAR-1996 <131172201+NIHAR-1996@users.noreply.github.com>
2023-06-17 11:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
-
+//1.Array of band names
+let bandNames=['The virupaksha temple','Victoria Memorial','Tajmahal'];
+//2.Remove Articles from Band Names by map Function
+let modifiednames=bandNames.map(names=>names.replace(/^(a|an|the)\s/i""));
+//3.Sorting the modified names lexicographically
+modifiednames.sort();
+//creating an HTML list
+let ul=document.createElement('ul');
+ul.setAttribute('id','band');
+modifiednames.forEach(name=>{
+	let li=document.createElemnt('li');
+	li.textContent=name;
+	ul.appendChild(li);
+})
